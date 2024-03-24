4aa93bebea8c02b0ab8a92cbc13aaf6d05352e35
congthanhhust <congthanh1368@gmail.com>
2024-03-24 20:14:29 +0700

    feat: Implement JWTService

---
+++ src/main/java/com/onemoretime/security/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.onemoretime.security.config;
+
+import com.onemoretime.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
