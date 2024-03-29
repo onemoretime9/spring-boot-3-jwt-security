b836f5a3771bd61539c130ade6fb8b0faa450f94
congthanhhust <congthanh1368@gmail.com>
2024-03-29 22:51:59 +0700

    feat: Add book service

---
+++ src/main/java/com/onemoretime/security/book/BookController.java
@@ -0,0 +1,29 @@
+package com.onemoretime.security.book;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService service;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody BookRequest request
+    ){
+        service.save(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAllBooks(){
+        return ResponseEntity.ok(service.findAll());
+    }
+}
---
+++ src/main/java/com/onemoretime/security/book/BookRepository.java
@@ -0,0 +1,6 @@
+package com.onemoretime.security.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
